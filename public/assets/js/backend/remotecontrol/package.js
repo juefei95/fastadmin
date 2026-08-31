5f2ae5f9c18a391eb025fbf455aee372611cdd91
zhenh <zhenh@DESKTOP-3B75NPC>
2026-08-31 17:30:36 +0800

    4生成CRUD代码

---
+++ public/assets/js/backend/remotecontrol/package.js
@@ -0,0 +1,61 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'remotecontrol/package/index' + location.search,
+                    add_url: 'remotecontrol/package/add',
+                    edit_url: 'remotecontrol/package/edit',
+                    del_url: 'remotecontrol/package/del',
+                    multi_url: 'remotecontrol/package/multi',
+                    import_url: 'remotecontrol/package/import',
+                    table: 'remote_package',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'weigh',
+                fixedColumns: true,
+                fixedRightNumber: 1,
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'name', title: __('Name'), operate: 'LIKE'},
+                        {field: 'days', title: __('Days')},
+                        {field: 'price', title: __('Price'), operate:'BETWEEN'},
+                        {field: 'recommended', title: __('Recommended'), searchList: {"0":__('Recommended 0'),"1":__('Recommended 1')}, formatter: Table.api.formatter.normal},
+                        {field: 'status', title: __('Status'), searchList: {"30":__('Status 30')}, formatter: Table.api.formatter.status},
+                        {field: 'weigh', title: __('Weigh'), operate: false},
+                        {field: 'description', title: __('Description'), operate: 'LIKE', table: table, class: 'autocontent', formatter: Table.api.formatter.content},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
