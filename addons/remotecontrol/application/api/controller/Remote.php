2702e44d63cd71c540a75e58d759aadb4f7b261a
zhenh <zhenh@DESKTOP-3B75NPC>
2026-09-01 00:27:17 +0800

    18开发创建订单 API

--- addons/remotecontrol/application/api/controller/Remote.php
+++ addons/remotecontrol/application/api/controller/Remote.php
@@ -2,6 +2,46 @@
 
 namespace app\api\controller;
 
+use addons\remotecontrol\library\RemoteOrderService;
+use think\Config;
+use think\Exception;
+
 class Remote extends remote\Index
 {
+    public function order()
+    {
+        if (!Config::get('fastadmin.usercenter')) {
+            $this->error(__('User center already closed'));
+        }
+        if (!$this->request->isPost() || strtolower($this->request->pathinfo()) !== 'api/remote/order/create') {
+            $this->error(__('Invalid parameters'));
+        }
+
+        $packageId = (int)$this->request->post('package_id');
+        $payType = (string)$this->request->post('pay_type', '');
+        if ($packageId <= 0) {
+            $this->error(__('Invalid parameters'));
+        }
+
+        try {
+            $service = new RemoteOrderService();
+            $order = $service->createPendingOrder($this->auth->id, $packageId, $payType);
+        } catch (Exception $e) {
+            $this->error($e->getMessage());
+        }
+
+        $this->success('', [
+            'order' => [
+                'id'           => (int)$order['id'],
+                'order_no'     => $order['order_no'],
+                'package_id'   => (int)$order['package_id'],
+                'package_name' => $order['package_name'],
+                'days'         => (int)$order['days'],
+                'amount'       => (string)$order['amount'],
+                'pay_type'     => $order['pay_type'],
+                'status'       => (int)$order['status'],
+                'createtime'   => (int)$order['createtime'],
+            ],
+        ]);
+    }
 }
