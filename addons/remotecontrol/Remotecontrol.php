ae88af5a7f99a8d1603ab9947c771e4a3f5dafbe
zhenh <zhenh@DESKTOP-3B75NPC>
2026-08-31 17:53:25 +0800

    7整理后台菜单

--- addons/remotecontrol/Remotecontrol.php
+++ addons/remotecontrol/Remotecontrol.php
@@ -17,7 +17,7 @@ class Remotecontrol extends Addons
      */
     public function install()
     {
-        
+        Menu::create($this->getMenu());
         return true;
     }
 
@@ -27,7 +27,7 @@ class Remotecontrol extends Addons
      */
     public function uninstall()
     {
-        
+        Menu::delete('remotecontrol');
         return true;
     }
 
@@ -37,7 +37,7 @@ class Remotecontrol extends Addons
      */
     public function enable()
     {
-        
+        Menu::enable('remotecontrol');
         return true;
     }
 
@@ -47,8 +47,68 @@ class Remotecontrol extends Addons
      */
     public function disable()
     {
-        
+        Menu::disable('remotecontrol');
         return true;
+    }
+
+    protected function getMenu()
+    {
+        return [
+            [
+                'name'    => 'remotecontrol',
+                'title'   => '远控管理',
+                'icon'    => 'fa fa-desktop',
+                'ismenu'  => 1,
+                'weigh'   => 100,
+                'sublist' => [
+                    [
+                        'name'    => 'remotecontrol/member',
+                        'title'   => '用户权益',
+                        'icon'    => 'fa fa-user',
+                        'ismenu'  => 1,
+                        'weigh'   => 40,
+                        'sublist' => $this->getCrudMenu('remotecontrol/member'),
+                    ],
+                    [
+                        'name'    => 'remotecontrol/package',
+                        'title'   => '套餐管理',
+                        'icon'    => 'fa fa-diamond',
+                        'ismenu'  => 1,
+                        'weigh'   => 30,
+                        'sublist' => $this->getCrudMenu('remotecontrol/package'),
+                    ],
+                    [
+                        'name'    => 'remotecontrol/order',
+                        'title'   => '订单管理',
+                        'icon'    => 'fa fa-list-alt',
+                        'ismenu'  => 1,
+                        'weigh'   => 20,
+                        'sublist' => $this->getCrudMenu('remotecontrol/order'),
+                    ],
+                    [
+                        'name'     => 'remotecontrol/config',
+                        'title'    => '插件设置',
+                        'icon'     => 'fa fa-cog',
+                        'url'      => 'addon/config?name=remotecontrol',
+                        'ismenu'   => 1,
+                        'menutype' => 'addtabs',
+                        'weigh'    => 10,
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    protected function getCrudMenu($name)
+    {
+        return [
+            ['name' => $name . '/index', 'title' => '查看'],
+            ['name' => $name . '/add', 'title' => '添加'],
+            ['name' => $name . '/edit', 'title' => '编辑'],
+            ['name' => $name . '/del', 'title' => '删除'],
+            ['name' => $name . '/multi', 'title' => '批量更新'],
+            ['name' => $name . '/dragsort', 'title' => '排序'],
+        ];
     }
 
 }
